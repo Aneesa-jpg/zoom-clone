64afe6075bd0a957a456cfda829ede55c8df9975
unknown <aneesa.farook.sait@gmail.com>
2020-10-02 21:42:10 +0530

    set up own peer-js server

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const bodyParser = require('body-parser')
-const { ExpressPeerServer } = require('peer')
-const peerServer = ExpressPeerServer(server, {
-  debug: true,
-})
+// const { ExpressPeerServer } = require('peer')
+// const peerServer = ExpressPeerServer(server, {
+//   debug: true,
+// })
 
-app.use('/peerjs', peerServer)
+// app.use('/peerjs', peerServer)
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
